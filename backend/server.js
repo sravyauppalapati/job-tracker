f790d4f0e6d870dad402c8aa234a0990c6af0f44
Sravya Uppalapati <sravyauppalapati16@gmail.com>
2026-03-04 15:01:12 +0530

    fix cors for new domain

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://job-tracker-teal-gamma.vercel.app'], // Vite dev server
+  origin: ['http://localhost:5173', 'https://job-tracker-teal-gamma.vercel.app', 'https://hiredyett.vercel.app'], // Vite dev server
   credentials: true,
 }));
 app.use(express.json());
